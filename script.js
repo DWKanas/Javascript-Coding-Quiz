de1661cf229e5386415b1ca0f7b9864e0fe23685
DWKanas <dkanas0122@gmail.com>
2022-06-02 23:13:16 -0700

    Javascript wasnt working, trying a different way

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ var questions = [
           {text: "4", correct: true},
           {text: "22", correct: false},
           {text: "18", correct: false},
-          {text: "4", correct: false}
+          {text: "5", correct: false}
       ]  
     },
     {
         question: "What is 5+5?",
         answers: [
-            {text: "2", correct: true},
-            {text: "10", correct: false},
+            {text: "2", correct: false},
+            {text: "10", correct: true},
             {text: "34", correct: false},
             {text: "8", correct: false}
         ]  
@@ -32,41 +32,77 @@ function startGame() {
     console.log("Started")
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestions = 0
-    nextQuestion()
+    askQuestion()
 }
 
-function nextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestions])
+function askQuestion() {
+    var incomingArrays = JSON.stringify(questions[0])
+    var incomingQuestions =JSON.parse(incomingArrays)
 
+     questionElement.innerHTML = incomingArrays.question
+    
+    console.log(incomingQuestions)
+    
 }
 
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer =>{
-        var button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("btn")
-        if (answer.correct) {
-           button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
 
-}
 
-function resetState() {
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// showQuestion(shuffledQuestions[currentQuestions])
+
+// nextQuestion()
+
+
+
+// resetState()
+
+
+
+
+
+
+
+// function showQuestion(question) {
+//     questionElement.innerText = question.question
+//     question.answers.forEach(answer =>{
+//         var button = document.createElement("button")
+//         button.innerText = answer.text
+//         button.classList.add("btn")
+//         button.addEventListener("click", selectAnswer)
+//         answerButtonsElement.appendChild(button)
+        
+//     })
+
+// }
+
+// function resetState() {
+
+// }
   
 
-function selectAnswer(e) {
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-       setStatusClass(button, button.dataset.correct) 
-    })
+// function selectAnswer(e) {
+//     var selectedButton = e.target
+//     var correct = selectedButton.dataset.correct
+//     setStatusClass(document.body, correct)
+//     Array.from(answerButtonsElement.children).forEach(button => {
+//        setStatusClass(button, button.dataset.correct) 
+//     })
 
-}
+// }
+
+// function setStatusClass() {
+    
+// }
