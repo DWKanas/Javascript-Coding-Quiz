d4b2f3fc726285f99285a0c005ab5be489f3d5f0
DWKanas <dkanas0122@gmail.com>
2022-06-04 16:10:13 -0700

    Fixed buttons

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var startButton = document.getElementById("startBtn")
 var questionElement = document.getElementById("question")
 var answerButtonsElement = document.getElementById("answerBtns")
 var shuffledQuestions, currentQuestions
-
+var btn1= document.getElementById("Btn1")
 var questions = [
     {
       question: "What is 2+2?",
@@ -29,26 +29,48 @@ var questions = [
 
 startButton.addEventListener("click", startGame)
 function startGame() {
-    console.log("Started")
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestions = 0
+    console.log("Started")
     askQuestion()
 }
 
 function askQuestion() {
-    var incomingArrays = JSON.stringify(questions[0])
-    var incomingQuestions =JSON.parse(incomingArrays)
-
-     questionElement.innerHTML = incomingArrays.question
-    
-    console.log(incomingQuestions)
-    
-}
-
-
-
+    // var incomingArrays = JSON.stringify(questions[0])
+    // var incomingQuestions =JSON.parse(incomingArrays)
+    var getQuestions = (questions[0].question);
+     questionElement.innerHTML = getQuestions
+        btn1.innerHTML = questions[0].answers[0].text
+     btn1.setAttribute("data-choice", questions[0].answers[0].correct)
+     
+     console.log(getQuestions)
+}//get attribute 
 
 
+// function showQuestion(question) {
+//     questionElement.innerText = question.question
+//     question.answers.forEach(answer =>{
+//         var button = document.createElement("button")
+//         button.innerText = answer.text
+//         button.classList.add("btn")
+//         button.addEventListener("click", selectAnswer)
+//         answerButtonsElement.appendChild(button)
+        
+//     })
+
+// }
+
+
+
+// function selectAnswer(e) {
+//     var selectedButton = e.target
+//     var correct = selectedButton.dataset.correct
+//     setStatusClass(document.body, correct)
+//     Array.from(answerButtonsElement.children).forEach(button => {
+//        setStatusClass(button, button.dataset.correct) 
+//     })
+
+// }
 
 
 
@@ -75,33 +97,12 @@ function askQuestion() {
 
 
 
-// function showQuestion(question) {
-//     questionElement.innerText = question.question
-//     question.answers.forEach(answer =>{
-//         var button = document.createElement("button")
-//         button.innerText = answer.text
-//         button.classList.add("btn")
-//         button.addEventListener("click", selectAnswer)
-//         answerButtonsElement.appendChild(button)
-        
-//     })
-
-// }
 
 // function resetState() {
 
 // }
   
 
-// function selectAnswer(e) {
-//     var selectedButton = e.target
-//     var correct = selectedButton.dataset.correct
-//     setStatusClass(document.body, correct)
-//     Array.from(answerButtonsElement.children).forEach(button => {
-//        setStatusClass(button, button.dataset.correct) 
-//     })
-
-// }
 
 // function setStatusClass() {
     
